44e53a5fc42f8001fcff30bb2644278a7f5660b7
Ian Graetzer <ian.graetzer@gmail.com>
2025-04-17 14:35:32 -0400

    label update

--- app.py
+++ app.py
@@ -367,7 +367,7 @@ with col3:
             
             summary_data.append({
                 "Race": race,
-                "Position": race_row['final_position'],
+                "Final Position": race_row['final_position'],
                 "Fastest Lap": race_row['fastest_lap'],
                 "Pit Stops": race_row['pit_stops']
             })
